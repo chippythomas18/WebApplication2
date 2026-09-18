b275c409f5b0f431cffdfb1e8745657ad746ea77
Chippy Thomas <chippythomas18@users.noreply.github.com>
2026-09-18 13:00:53 -0400

    added animation and css changes and youtube link

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let animationId = null;
+
+function myMove() {
+    const elem = document.getElementById("myAnimation");
+    let pos = 0;
+
+    // Stops any active animation if you click the button again midway
+    clearInterval(animationId);
+
+    animationId = setInterval(frame, 5);
+
+    function frame() {
+        if (pos == 350) {
+            clearInterval(animationId);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
